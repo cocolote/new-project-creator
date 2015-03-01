a35d0e2391b4328bfe7d9c3b4eb74f8fe9d3184e
Ezequiel Jose Lopez <skiel.j.lopez@gmail.com>
2015-03-01 10:51:59 -0500

    working application

--- new_project.rb
+++ new_project.rb
@@ -1,8 +1,8 @@
 # this is a simple routine to create the basic directories for a Sinatra
 # application,
-ARGV.empty? ? path = "./new-project" : path = ARGV.join
+ARGV.empty? ? project_name = "new-project" : project_name = ARGV.join
 
-file_name = path.scan(/\.\/([\w\d_\-]+)\z/i).join
+path = "#{File.expand_path(".").split('/')[0..-2].join('/')}/#{project_name}"
 
 directories = [
   "#{path}/views",
@@ -11,7 +11,7 @@ directories = [
   "#{path}/public/js/vendor",
   "#{path}/public/css",
   "#{path}/public/css/vendor"
-              ]
+  ]
 
 files = {
   "#{path}/.gitignore" => [
@@ -19,8 +19,7 @@ files = {
     ],
   "#{path}/Gemfile" => [
     "#share all the gems that your awesome app needs",
-    "source 'https://rubygems.org/'",
-    "",
+    "source 'https://rubygems.org/'\n\n",
     "gem 'sinatra'"],
   "#{path}/server.rb" => [
     "require 'sinatra'",
@@ -28,19 +27,45 @@ files = {
     "#this is dedicated to Spencer, great EE who show us the power of pry!",
     "require 'pry'\n\n",
     "get '/' do",
-    "  redirect '/#here goes your name of choice'",
+    "  redirect '/home'",
+    "end\n\n",
+    "get '/home' do",
+    "  erb :home",
     "end"],
-  "#{path}/public/js/#{file_name}.js" => [
+  "#{path}/public/js/#{project_name}.js" => [
     "// The canvas for your awesome JS code"
     ],
-  "#{path}/public/css/#{file_name}.css" =>[
+  "#{path}/public/css/#{project_name}.css" =>[
     "/* Let's put some makeup! */"
     ],
   "#{path}/views/layout.erb" => [
-    "<!-- The foundations of your website serious stuff -->",
-    "<!-- TIP: if you have the package Emmet installed puts a ! and then press tab -->"
+    "<!DOCTYPE html>",
+    "<html lang='en'>",
+    "<head>",
+    "  <meta charset='UTF-8'>",
+    "  <title>My New Awesome Application</title>",
+    "  <link rel='stylesheet' href='/css/vendor/foundation.min.css'>",
+    "  <link rel='stylesheet' href='/css/vendor/normalize.css'>",
+    "  <link rel='stylesheet' href='/css/#{project_name}.css'>",
+    "</head>",
+    "<body>",
+    "  <div class='row'>\n\n",
+    "    <%= yield %>\n\n",
+    "  </div>",
+    "  <script src='/js/vendor/jquery-2.1.3.min.js'></script>",
+    "  <script src='/#{project_name}.js'></script>",
+    "</body>",
+    "</html>"],
+  "#{path}/views/home.erb" => [
+    "<h1>My New Awesome Application</h1>\n\n",
   ]
 }
+
+vendor_files = [
+  "foundation.min.css",
+  "normalize.css",
+  "jquery-2.1.3.min.js"
+  ]
 
 directories.each { |path_dir| system 'mkdir', '-p', path_dir }
 
@@ -50,4 +75,9 @@ files.each do |file_path, info|
   File.open(file_path, 'w') do |file|
     info.each { |line| file.puts(line) }
   end
+end
+
+vendor_files.each do |vfile_name|
+  extension = vfile_name.scan(/\.(\w{2,3})\z/i).join
+  system 'cp', "./public/#{vfile_name}", "#{path}/public/#{extension}/vendor/"
 end
