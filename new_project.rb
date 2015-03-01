d60691e41bb68ecafcc34b600588e6257add91d4
Ezequiel Jose Lopez <skiel.j.lopez@gmail.com>
2015-03-01 12:49:29 -0500

    database connections and request

--- new_project.rb
+++ new_project.rb
@@ -13,31 +13,77 @@ directories = [
   "#{path}/public/css/vendor"
   ]
 
+# If you need to use more files just added here in the hash
+# like key: "#{path}/<name of the file/" => ["<lines that you want in the file>"]
 files = {
+  # SQL schema file to create the tables in your database
+    "#{path}/schema.sql" => [
+    "-- If you want to run this schema repeatedly you'll need to drop",
+    "-- the table before re-creating it. Note that you'll lose any",
+    "-- data if you drop and add a table:\n\n",
+    "-- DROP TABLE IF EXISTS <table name>;\n\n",
+    "-- Define your schema here:\n\n",
+    "-- CREATE TABLE <table name> (",
+    "--   id serial PRIMARY KEY,",
+    "--   <column name> <type of data> NOT NULL",
+    "-- );",
+    ],
+ # .gitignore file to hide your private information like API keys
   "#{path}/.gitignore" => [
     ".env"
     ],
+ # Gemfile add all your gems to share with your app
   "#{path}/Gemfile" => [
     "#share all the gems that your awesome app needs",
     "source 'https://rubygems.org/'\n\n",
-    "gem 'sinatra'"],
+    "gem 'sinatra'",
+    "gem 'pg'"
+    ],
+ # The heart of your app, functions and controllers
   "#{path}/server.rb" => [
     "require 'sinatra'",
     "require 'erb'",
     "#this is dedicated to Spencer, great EE who show us the power of pry!",
-    "require 'pry'\n\n",
+    "require 'pry'",
+    "#Uncomment this line to use 'pg' to connect to your DB",
+    "#require 'pg'\n\n",
+    "#############",
+    "# FUNCTIONS #",
+    "#############\n\n",
+    "#connection to your database using postgreSQL",
+    "#def db_connection",
+    "#  begin",
+    '#    connection = PG.connect(dbname: "<database name>")',
+    "#    yield(connection)",
+    "#  ensure",
+    "#    connection.close",
+    "#  end",
+    "#end\n\n",
+    "#request the data from your table",
+    "#def get_data",
+    "#  <your variable> = db_connection do |conn|",
+    '#    conn.exec("SELECT <rows names> FROM <table name>")',
+    "#  end",
+    "#end\n\n",
+    "###############",
+    "# CONTROLLERS #",
+    "###############\n\n",
     "get '/' do",
     "  redirect '/home'",
     "end\n\n",
     "get '/home' do",
     "  erb :home",
-    "end"],
+    "end"
+    ],
+ # Your own JavaScript file
   "#{path}/public/js/#{project_name}.js" => [
     "// The canvas for your awesome JS code"
     ],
+ # Your own CSS file
   "#{path}/public/css/#{project_name}.css" =>[
     "/* Let's put some makeup! */"
     ],
+ # The layout file already linked to the CSS files and JS
   "#{path}/views/layout.erb" => [
     "<!DOCTYPE html>",
     "<html lang='en'>",
@@ -55,7 +101,8 @@ files = {
     "  <script src='/js/vendor/jquery-2.1.3.min.js'></script>",
     "  <script src='/#{project_name}.js'></script>",
     "</body>",
-    "</html>"],
+    "</html>"
+    ],
   "#{path}/views/home.erb" => [
     "<h1>My New Awesome Application</h1>\n\n",
   ]
